e1d86a78f4855ceb271a1283acbfef930f2bbe12
younes <younes@192.168.1.34>
2017-03-06 21:36:47 +0100

    fix(service): fix the return of the method

--- TestTechniqueEnedis/src/main/java/FizzAndBuzz.java
+++ TestTechniqueEnedis/src/main/java/FizzAndBuzz.java
@@ -1,9 +1,6 @@
 
 public class FizzAndBuzz {
-	 public static String getResult(int number) {
-	    if (number % 15 == 0) return "fizzbuzz";
-	    else if (number % 3 == 0) return "fizz";
-	    else if (number % 5 == 0) return "buzz";
-	    return Integer.toString(number);
-	}
+	 public static String divisionResult(int number) {
+	        if (number % 15 == 0) return "fizzbuzz";
+	 }
 }
