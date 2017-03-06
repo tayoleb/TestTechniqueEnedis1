a3c99713f77773fbc90738edbb14593523afbcbf
younes <younes@192.168.1.34>
2017-03-06 21:32:17 +0100

    feature(service): add the method to return if the number is divided by 15

--- TestTechniqueEnedis/src/main/java/FizzAndBuzz.java
+++ TestTechniqueEnedis/src/main/java/FizzAndBuzz.java
@@ -1,4 +1,9 @@
 
 public class FizzAndBuzz {
-
+	 public static String getResult(int number) {
+	    if (number % 15 == 0) return "fizzbuzz";
+	    else if (number % 3 == 0) return "fizz";
+	    else if (number % 5 == 0) return "buzz";
+	    return Integer.toString(number);
+	}
 }
