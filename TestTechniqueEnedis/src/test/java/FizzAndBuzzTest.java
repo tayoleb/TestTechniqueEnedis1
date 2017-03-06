1d694cda5d0538306420e4808cbcc92178470409
younes <younes@192.168.1.34>
2017-03-06 21:24:10 +0100

    test(test) : add the test for the dividing on 15

--- TestTechniqueEnedis/src/test/java/FizzAndBuzzTest.java
+++ TestTechniqueEnedis/src/test/java/FizzAndBuzzTest.java
@@ -1,19 +1,16 @@
-import static org.junit.Assert.*;
+
+
 
 import org.junit.Assert;
 import org.junit.Test;
 
+
 public class FizzAndBuzzTest {
 
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
-	@Test
-    public void getResultShouldReturnTheSameNumberIfNoOtherRequirementIsFulfilled() {
-        Assert.assertEquals("1", FizzAndBuzz.getResult(1));
-        Assert.assertEquals("2", FizzAndBuzz.getResult(2));
-        Assert.assertEquals("4", FizzAndBuzz.getResult(4));
-    }
+	 @Test
+	    public void getResultWhenDividedBy15() {
+	        Assert.assertEquals("fizzbuzz", FizzAndBuzz.divisionResult(15));
+	        Assert.assertEquals("fizzbuzz", FizzAndBuzz.divisionResult(30));
+	    }
 
 }
